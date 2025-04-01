206dbf9e0bd090a367bd81216c91b37b83487ffc
Kiril Vuchkov <kirilvuchkov@gmail.com>
2025-04-01 06:30:23 +0000

    Basic disocunt and functinality

--- src/discount.cpp
+++ src/discount.cpp
@@ -3,3 +3,13 @@
 PercentageDiscount::PercentageDiscount(int percentage) : off(percentage / 100.0)
 {
 }
+
+double PercentageDiscount::apply(double total)
+{
+    return total - off * total;
+}
+
+void PercentageDiscount::print(std::ostream &out)
+{
+    out << "Discount: " << int(off * 100.0) << "% off" << std::endl;
+}
--- src/item.h
+++ src/item.h
@@ -8,7 +8,8 @@ class Item
 {
 public:
     Item(const string &name, double price);
-
+    string getName() const;
+    double getPrice() const;
 private:
     string name;
     double price;
